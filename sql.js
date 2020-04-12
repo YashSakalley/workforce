70d05acb1c82a79019ac6a68caabc5b6e46055da
Yash Sakalley <yashsakalley98@gmail.com>
2020-04-12 20:29:31 +0530

    Added online MySQL

--- sql.js
+++ sql.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
-    host: 'localhost',
-    database: 'workforce',
-    user: 'root',
-    password: 'Mangotree@123'
+    host: 'sql12.freemysqlhosting.net',
+    database: 'sql12332989',
+    user: 'sql12332989',
+    password: 'N9UgDC16BG'
 });
 
 con.connect(function (err) {
@@ -12,26 +12,69 @@ con.connect(function (err) {
     console.log('Connected');
 });
 
-/*
-q = 'SELECT * FROM users where email_id = ?';
-email_id = 'yashsakalley98@gmail.com';
+q1 = "CREATE TABLE users ( " +
+    "id INT AUTO_INCREMENT PRIMARY KEY, " +
+    "first_name VARCHAR(255), " +
+    "last_name VARCHAR(255), " +
+    "email_id VARCHAR(255) UNIQUE NOT NULL, " +
+    "password VARCHAR(255) NOT NULL, " +
+    "phone_number VARCHAR(10) UNIQUE NOT NULL, " +
+    "address VARCHAR(255), " +
+    "profile_photo VARCHAR(255), " +
+    "created_at TIMESTAMP DEFAULT NOW() " +
+    ");";
 
-con.query(q, [email_id], function (err, user) {
+q2 = "CREATE TABLE workers ( " +
+    "id INT AUTO_INCREMENT PRIMARY KEY, " +
+    "first_name VARCHAR(255) NOT NULL, " +
+    "last_name VARCHAR(255) NOT NULL, " +
+    "email_id VARCHAR(255) UNIQUE NOT NULL, " +
+    "password VARCHAR(255) NOT NULL, " +
+    "phone_number VARCHAR(10) UNIQUE NOT NULL, " +
+    "shop_address VARCHAR(255) NOT NULL, " +
+    "profile_photo VARCHAR(255), " +
+    "created_at TIMESTAMP DEFAULT NOW(), " +
+    "average_rating FLOAT, " +
+    "total_reviews INT, " +
+    "job VARCHAR(50) UNIQUE " +
+    ");";
+
+q3 = "CREATE TABLE reviews ( " +
+    "id INT AUTO_INCREMENT PRIMARY KEY, " +
+    "rating INT, " +
+    "reviews_text VARCHAR(255), " +
+    "worker_id INT, " +
+    "user_id INT, " +
+    "created_at TIMESTAMP DEFAULT NOW(), " +
+    "FOREIGN KEY(user_id) REFERENCES users(id), " +
+    "FOREIGN KEY(worker_id) REFERENCES workers(id) " +
+    ");"
+
+q4 = "CREATE TABLE requests ( " +
+    "id INT AUTO_INCREMENT PRIMARY KEY, " +
+    "created_at TIMESTAMP DEFAULT NOW(), " +
+    "user_id INT NOT NULL, " +
+    "worker_id INT NOT NULL," +
+    "current_status VARCHAR(20), " +
+    "job VARCHAR(50), " +
+    "address VARCHAR(255), " +
+    "FOREIGN KEY(user_id) REFERENCES users(id), " +
+    "FOREIGN KEY(worker_id) REFERENCES workers(id) " +
+    ");";
+
+q5 = "CREATE TABLE temp_requests ( " +
+    "id INT AUTO_INCREMENT PRIMARY KEY, " +
+    "user_id INT NOT NULL, " +
+    "created_at TIMESTAMP DEFAULT NOW(), " +
+    "address VARCHAR(255), " +
+    "current_status VARCHAR(20), " +
+    "job VARCHAR(50) NOT NULL, " +
+    "FOREIGN KEY(user_id) REFERENCES users(id) " +
+    ");"
+
+con.query(q5, function (err, records, fields) {
     if (err) throw err;
-
-    if (user.length == 0) {
-        console.log('No user found');
-    }
-    else {
-        console.log('Found user', user[0]);
-    }
-});
-*/
-
-q = 'DELETE FROM temp_requests WHERE id = ?';
-con.query(q, 1, function (err, records, fields) {
-    console.log('records', records);
-    console.log('fields', fields);
+    console.log(records);
 });
 
 con.end();
